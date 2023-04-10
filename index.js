ee7e0db7d1f86513d9f5ce8c3d204373ce457f34
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-04-09 22:53:17 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(nameArray){
+   const newArray = nameArray.slice(0,2);
+   return newArray;
+}
+
+const returnLastTwoDrivers = function(nameArray){
+    const newArray = nameArray.slice(-2)
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){return multiplier*fare}
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(names, driverFunction){
+    return driverFunction(names);
+}
